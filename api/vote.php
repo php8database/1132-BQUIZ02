d30c89be068e6bc47ae2fca1ad79ce182a872d5a
php8database <php8database@gmail.com>
2025-01-10 12:51:52 +0800

    更新 api/vote.php 與 front/vote.php 怎麼禁止空按我要投票？

--- api/vote.php
+++ api/vote.php
@@ -1,11 +1,22 @@
 <?php include_once "db.php";
 
-$opt_id=$_POST['opt'];
-$option=$Que->find($opt_id);
-$subject=$Que->find($option['main_id']);
+// 檢查是否有選擇選項
+if (!isset($_POST['opt']) || empty($_POST['opt'])) {
+    echo "請選擇一個選項！";
+    exit; // 停止執行，提示使用者選擇選項
+}
+
+$opt_id = $_POST['opt'];
+$option = $Que->find($opt_id);
+$subject = $Que->find($option['main_id']);
+
+// 更新投票數
 $option['vote']++;
 $subject['vote']++;
+
+// 儲存更新的資料
 $Que->save($option);
 $Que->save($subject);
 
+// 重定向到結果頁面
 to("../index.php?do=result&id={$option['main_id']}");
--- front/vote.php
+++ front/vote.php
@@ -1,13 +1,13 @@
 <?php 
 
-$subject=$Que->find($_GET['id']);
-$options=$Que->all(['main_id'=>$_GET['id']]);
+$subject = $Que->find($_GET['id']);
+$options = $Que->all(['main_id' => $_GET['id']]);
 ?>
 
 <fieldset>
     <legend>目前位置：首頁 > 問卷調查 > <?=$subject['text'];?></legend>
 <h3><?=$subject['text'];?></h3>
-<form action="./api/vote.php" method="post">
+<form action="./api/vote.php" method="post" onsubmit="return checkVote()">
 <?php 
 foreach($options as $option){
     echo "<p>";
@@ -19,7 +19,39 @@ foreach($options as $option){
 <div class="ct">
     <input type="submit" value="我要投票">
 </div>
+
+<!-- 顯示錯誤訊息的區塊 -->
+<div id="error-message" style="color: red; display: none;">
+    請選擇一個選項！
+</div>
+
 </form>
+
+<script>
+function checkVote() {
+    var radios = document.getElementsByName('opt');
+    var isChecked = false;
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+    if (!isChecked) {
+        // 顯示錯誤訊息
+        document.getElementById("error-message").style.display = "block";
+        
+        // 設定1秒後重新載入頁面
+        setTimeout(function() {
+			location.href = '?do=que'; // 返回問卷頁面首頁
+            //location.reload(); // 重新載入當前頁面
+        }, 1000); // 2000毫秒即2秒後執行
+
+        return false; // 阻止表單提交
+    }
+    return true; // 允許提交
+}
+</script>
     
 
 </fieldset>
