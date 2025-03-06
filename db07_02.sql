1b18760c04a4291d39ae96c6b6b0027800ea3d1d
php8database <php8database@gmail.com>
2025-03-06 11:10:13 +0800

    更新資料庫

--- db07_02.sql
+++ db07_02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-01-03 08:28:55
+-- 產生時間： 2025-03-06 04:09:58
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -72,25 +72,26 @@ CREATE TABLE `que` (
   `id` int(10) UNSIGNED NOT NULL,
   `text` text NOT NULL,
   `main_id` int(10) UNSIGNED NOT NULL,
-  `vote` int(10) UNSIGNED NOT NULL
+  `vote` int(10) UNSIGNED NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 傾印資料表的資料 `que`
 --
 
-INSERT INTO `que` (`id`, `text`, `main_id`, `vote`) VALUES
-(1, '問題一：你最常做什麼運動來促進健康體能呢?', 0, 0),
-(2, '1.健走或爬樓梯、慢跑等較不受時間、場地限制的運動。', 1, 0),
-(3, '2.仰臥起坐、抬腿及伏地挺身、伸展操、瑜珈等室內運動。', 1, 0),
-(4, '3.球類運動、游泳、跳舞、騎腳踏車等加強心肺功能的運動。', 1, 0),
-(5, '4.舉重鍛鍊、彈力帶、啞鈴等運用輔助器材鍛鍊肌耐力的運動。', 1, 0),
-(6, '問題二：二手菸沒有安全劑量，只要有暴露，就會有危險，請問它會造成身體上哪些危害?', 0, 0),
-(7, '1.增加罹患冠狀動脈心臟病及罹病死亡之風險。', 6, 0),
-(8, '2.對孩子的的健康會產生許多影響，例如容易咳嗽或打噴嚏、罹患氣喘或讓症狀更嚴重、會因刺激耳咽管，感染中耳炎、肺功能較差、容易罹患呼吸道疾病等。', 6, 0),
-(9, '3.孕婦吸入二手菸易造成流產、早產、胎盤早期剝離、子宮感染等疾病。', 6, 0),
-(10, '4.長期的暴露會造成更嚴重的胸腔問題和過敏症，還會增加心臟病和肺癌的罹患率。', 6, 0),
-(11, '5.以上皆是。', 6, 0);
+INSERT INTO `que` (`id`, `text`, `main_id`, `vote`, `sh`) VALUES
+(1, '問題一：你最常做什麼運動來促進健康體能呢?', 0, 14, 1),
+(2, '1.健走或爬樓梯、慢跑等較不受時間、場地限制的運動。', 1, 4, 1),
+(3, '2.仰臥起坐、抬腿及伏地挺身、伸展操、瑜珈等室內運動。', 1, 4, 1),
+(4, '3.球類運動、游泳、跳舞、騎腳踏車等加強心肺功能的運動。', 1, 3, 1),
+(5, '4.舉重鍛鍊、彈力帶、啞鈴等運用輔助器材鍛鍊肌耐力的運動。', 1, 3, 1),
+(6, '問題二：二手菸沒有安全劑量，只要有暴露，就會有危險，請問它會造成身體上哪些危害?', 0, 14, 1),
+(7, '1.增加罹患冠狀動脈心臟病及罹病死亡之風險。', 6, 2, 1),
+(8, '2.對孩子的的健康會產生許多影響，例如容易咳嗽或打噴嚏、罹患氣喘或讓症狀更嚴重、會因刺激耳咽管，感染中耳炎、肺功能較差、容易罹患呼吸道疾病等。', 6, 1, 1),
+(9, '3.孕婦吸入二手菸易造成流產、早產、胎盤早期剝離、子宮感染等疾病。', 6, 1, 1),
+(10, '4.長期的暴露會造成更嚴重的胸腔問題和過敏症，還會增加心臟病和肺癌的罹患率。', 6, 2, 1),
+(11, '5.以上皆是。', 6, 8, 1);
 
 -- --------------------------------------------------------
 
@@ -100,8 +101,8 @@ INSERT INTO `que` (`id`, `text`, `main_id`, `vote`) VALUES
 
 CREATE TABLE `total` (
   `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
-  `date` date NOT NULL COMMENT '訪客人數',
-  `total` int(10) UNSIGNED NOT NULL COMMENT '日期'
+  `date` date NOT NULL COMMENT '日期',
+  `total` int(10) UNSIGNED NOT NULL COMMENT '訪客人數'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -109,9 +110,8 @@ CREATE TABLE `total` (
 --
 
 INSERT INTO `total` (`id`, `date`, `total`) VALUES
-(1, '2024-12-30', 1),
-(2, '2025-01-02', 2),
-(3, '2025-01-03', 1);
+(1, '2025-03-04', 1),
+(2, '2025-03-06', 1);
 
 -- --------------------------------------------------------
 
@@ -133,8 +133,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `acc`, `pw`, `email`) VALUES
 (1, 'test', '5678', 'test@'),
 (2, 'admin', '1234', 'admin@'),
-(3, 'mem01', 'mem01', 'mem01@'),
-(4, 'mem02', 'mem02', 'mem02@');
+(5, 'mem01', '1234', 'mem01.test@gmail.com'),
+(6, 'mem02', '5678', 'mem02.test@gmail.com');
 
 --
 -- 已傾印資料表的索引
@@ -184,25 +184,25 @@ ALTER TABLE `log`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `que`
 --
 ALTER TABLE `que`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `total`
 --
 ALTER TABLE `total`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
